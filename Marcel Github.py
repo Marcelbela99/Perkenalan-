796e73d834fa3aa7c70580ce046f5acb0eb03776
Marcel Sintia Bela <94430658+Marcelbela99@users.noreply.github.com>
2021-11-16 14:05:33 +0700

    Add files via upload

---
+++ Marcel Github.py
@@ -0,0 +1,6 @@
+Nama = "Marcel Sintia Bela"
+NIM  = "212410102027"
+Asal = "Banyuwangi"
+Kelompok = "9"
+Hobi = "Menyanyanyi"
+print("Halo TI Fams kenalin namaku", Nama , "Dengan NIM" , NIM , "Asal", Asal, "Dari Kelompok", Kelompok, " dan aku mempunyai hobi yaitu", Hobi, "Sekian Terimakasih.")
